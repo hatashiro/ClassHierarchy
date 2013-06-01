ccdf544f9e3b522aeb874d21bdda480b1d6527df
HyeonJe Jun <noraesae@yuiazu.net>
2013-06-02 00:31:01 +0900

    Change the logic to get setting.

--- settings.py
+++ settings.py
@@ -1,18 +1,36 @@
 # -*- encoding: utf-8 -*-
 import sublime
 
-def get_settings():
-    return sublime.load_settings("ClassHierarchy.sublime-settings")
+def get_setting(settings, key):
+    if settings.has(key):
+        return settings.get(key)
+    else:
+        return None
 
-def get_setting(key, default=None, view=None):
+def get_setting_from_view(key):
     try:
-        if view == None:
-            view = sublime.active_window().active_view()
-        s = view.settings()
-        if s.has("class_hierarchy_%s" % key):
-            return s.get("class_hierarchy_%s" % key)
+        view = sublime.active_window().active_view()
+        return get_setting(view.settings(), "class_hierarchy_%s" % key)
     except:
-        pass
-    return get_settings().get(key, default)
+        return None
 
-setting = get_setting
+def get_setting_from_preferences(key):
+    settings = sublime.load_settings("Preferences.sublime-settings")
+    return get_setting(settings, "class_hierarchy_%s" % key)
+
+def get_setting_from_plugin(key):
+    settings = sublime.load_settings("ClassHierarchy.sublime-settings")
+    return get_setting(settings, key)
+
+def setting(key, default=None, view=None):
+    value = get_setting_from_view(key)
+    if value:
+        return value
+
+    value = get_setting_from_preferences(key)
+    if value:
+        return value
+
+    value = get_setting_from_plugin(key)
+    if value:
+        return value
